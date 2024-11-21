b8dac0a126a05333d53c2b93a97e07f59a2878eb
Ambika Mohakud <158445221+ambi-02@users.noreply.github.com>
2024-11-21 09:48:47 +0530

    Time: 2 ms (55.39%), Space: 44.7 MB (74.81%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,24 +1,41 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int maxi = Integer.MIN_VALUE;
-    //     int pro=1;
-    //    for(int i=0;i<nums.length; i++ ){
-    //      pro = pro*nums[i];
+        // int maxi = Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length; i++ ){
+        //    int pro=1;
+        //     for(int j=i; j<nums.length; j++){
+        //         pro = pro*nums[j];
+        //         maxi = Math.max(maxi, pro);
+        //     }
+        // }
+        //  return maxi;
+              // Edge case: if the array is empty
+        if (nums.length == 0) return 0;
 
-    //      if(pro>maxi){
-    //         maxi= pro;
-    //      }
-    //      if(pro<0){
-    // //         pro=1;
-    // //      }
-    //    }
-        for(int i=0;i<nums.length; i++ ){
-           int pro=1;
-            for(int j=i; j<nums.length; j++){
-                pro = pro*nums[j];
-                maxi = Math.max(maxi, pro);
+        // Initialize variables
+        int maxProduct = nums[0];
+        int currentMax = nums[0];
+        int currentMin = nums[0];
+
+        // Iterate through the array starting from the second element
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+
+            // If the current number is negative, swap currentMax and currentMin
+            if (num < 0) {
+                int temp = currentMax;
+                currentMax = currentMin;
+                currentMin = temp;
             }
+
+            // Calculate the maximum and minimum products
+            currentMax = Math.max(num, currentMax * num);
+            currentMin = Math.min(num, currentMin * num);
+
+            // Update the overall maximum product
+            maxProduct = Math.max(maxProduct, currentMax);
         }
-         return maxi;
+
+        return maxProduct;
     }
-}
+    }
