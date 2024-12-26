50f1826a0c3495a9f40b4aba508c846f21768e74
Ambika Mohakud <158445221+ambi-02@users.noreply.github.com>
2024-12-26 15:52:04 +0530

    Time: 1 ms (99.9%), Space: 48.8 MB (38.21%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -12,6 +12,8 @@ import java.util.HashSet;
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+//APPROACH 1 
         // HashSet<ListNode> st = new HashSet<>();
         
         // // Traverse the first list and add all nodes to the HashSet
@@ -30,33 +32,45 @@ public class Solution {
         
         // return null; // No intersection
 
-        int diff= getDiff(headA,headB);
-        if(diff<0){
-            while(diff++!=0) headB=headB.next;
-        }else{
-            while(diff--!=0) headA=headA.next;
-        }
-        while(headA!=null){
-            if(headA==headB) return headA;
+// APPROACH 2
+    //     int diff= getDiff(headA,headB);
+    //     if(diff<0){
+    //         while(diff++!=0) headB=headB.next;
+    //     }else{
+    //         while(diff--!=0) headA=headA.next;
+    //     }
+    //     while(headA!=null){
+    //         if(headA==headB) return headA;
 
-            headA=headA.next;
-            headB=headB.next;
+    //         headA=headA.next;
+    //         headB=headB.next;
+    //     }
+    //     return headA;
+    // }
+    // public static int getDiff(ListNode headA, ListNode headB){
+    //     int n1=0;
+    //     int n2=0;
+    //     while(headA!=null || headB!=null){
+    //         if(headA!=null){
+    //             n1++;
+    //             headA=headA.next;
+    //         }
+    //          if(headB!=null){
+    //             n2++;
+    //             headB=headB.next;
+    //         }
+    //     }
+    //     return n1-n2;
+    // }
+
+//  APPROACH 3
+        ListNode t1=headA;
+        ListNode t2=headB;
+
+        while(t1!=t2){
+            t1 = t1 == null? headB:t1.next;
+        t2 = t2 == null? headA:t2.next;
         }
-        return headA;
-    }
-    public static int getDiff(ListNode headA, ListNode headB){
-        int n1=0;
-        int n2=0;
-        while(headA!=null || headB!=null){
-            if(headA!=null){
-                n1++;
-                headA=headA.next;
-            }
-             if(headB!=null){
-                n2++;
-                headB=headB.next;
-            }
-        }
-        return n1-n2;
+return t1;
     }
 }
