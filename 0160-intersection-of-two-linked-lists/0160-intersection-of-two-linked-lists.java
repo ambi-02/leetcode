a56250c30d4f742c14fa55582492e2acf08a44ef
Ambika Mohakud <158445221+ambi-02@users.noreply.github.com>
2024-12-26 15:04:28 +0530

    Time: 1 ms (99.9%), Space: 49.2 MB (10.77%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -12,22 +12,51 @@ import java.util.HashSet;
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashSet<ListNode> st = new HashSet<>();
+        // HashSet<ListNode> st = new HashSet<>();
         
-        // Traverse the first list and add all nodes to the HashSet
-        while (headA != null) {
-            st.add(headA);
-            headA = headA.next;
+        // // Traverse the first list and add all nodes to the HashSet
+        // while (headA != null) {
+        //     st.add(headA);
+        //     headA = headA.next;
+        // }
+        
+        // // Traverse the second list and check for intersection
+        // while (headB != null) {
+        //     if (st.contains(headB)) {
+        //         return headB; // Intersection found
+        //     }
+        //     headB = headB.next;
+        // }
+        
+        // return null; // No intersection
+
+        int diff= getDiff(headA,headB);
+        if(diff<0){
+            while(diff++!=0) headB=headB.next;
+        }else{
+            while(diff--!=0) headA=headA.next;
         }
-        
-        // Traverse the second list and check for intersection
-        while (headB != null) {
-            if (st.contains(headB)) {
-                return headB; // Intersection found
+        while(headA!=null){
+            if(headA==headB) return headA;
+
+            headA=headA.next;
+            headB=headB.next;
+        }
+        return headA;
+    }
+    public static int getDiff(ListNode headA, ListNode headB){
+        int n1=0;
+        int n2=0;
+        while(headA!=null || headB!=null){
+            if(headA!=null){
+                n1++;
+                headA=headA.next;
             }
-            headB = headB.next;
+             if(headB!=null){
+                n2++;
+                headB=headB.next;
+            }
         }
-        
-        return null; // No intersection
+        return n1-n2;
     }
 }
