086e219df121b7f64150e55828ce9ff21ed68561
Ambika Mohakud <158445221+ambi-02@users.noreply.github.com>
2025-01-02 10:20:51 +0530

    Time: 5 ms (49.81%), Space: 42.2 MB (71.37%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,23 +1,37 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        StringBuilder res = new StringBuilder();
-        int depth = 0;
+        // StringBuilder res = new StringBuilder();
+        // int depth = 0;
 
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                // Before we increase depth, check if it's not the outermost '('
-                if (depth > 0) {
-                    res.append(ch); // Append if it's not the outermost '('
-                }
-                depth++; // Increment depth for '('
-            } else { // ch == ')'
-                depth--; // Decrement depth for ')'
-                // Append if it's not the outermost ')'
-                if (depth > 0) {
-                    res.append(ch);
-                }
+        // for (char ch : s.toCharArray()) {
+        //     if (ch == '(') {
+        //         // Before we increase depth, check if it's not the outermost '('
+        //         if (depth > 0) {
+        //             res.append(ch); // Append if it's not the outermost '('
+        //         }
+        //         depth++; // Increment depth for '('
+        //     } else { // ch == ')'
+        //         depth--; // Decrement depth for ')'
+        //         // Append if it's not the outermost ')'
+        //         if (depth > 0) {
+        //             res.append(ch);
+        //         }
+        //     }
+        // }
+        // return res.toString(); // Corrected: Added parentheses to toString()
+        int count = 0;
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                count--;
+            }
+            if (count != 0) {
+                ans.append(s.charAt(i));
+            }
+            if (s.charAt(i) == '(') {
+                count++;
             }
         }
-        return res.toString(); // Corrected: Added parentheses to toString()
+        return ans.toString();
     }
 }
